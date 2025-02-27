3eaceda0077893aac4d2a3eccfbce806820a14ab
Aaron Francis <Aarondfrancis@gmail.com>
2025-02-27 15:29:32 -0600

    Kill the underlying process (#79)
    * Kill the underlying process
    
    * Fix code styling [ci skip]
    
    ---------
    
    Co-authored-by: aarondfrancis <881931+aarondfrancis@users.noreply.github.com>

--- src/Commands/Concerns/ManagesProcess.php
+++ src/Commands/Concerns/ManagesProcess.php
@@ -60,7 +60,7 @@ trait ManagesProcess
             ->command([
                 'bash',
                 '-c',
-                "stty cols {$screen->width} rows {$screen->height} && screen -q " . $this->command,
+                "stty cols {$screen->width} rows {$screen->height} && screen -m -q {$this->command}",
             ])
             ->forever()
             ->timeout(0)
@@ -179,9 +179,14 @@ trait ManagesProcess
             // Keep track of when we tried to stop.
             $this->stopInitiatedAt ??= Carbon::now();
 
-            // Ask for a graceful shutdown. If it isn't
-            // respected, we'll force kill it later.
-            $this->process->signal(SIGTERM);
+            foreach ($this->children as $pid) {
+                $command = trim(shell_exec("ps -o command= -p $pid"));
+
+                // If it doesn't contain 'screen' or 'SCREEN', it's likely our actual command
+                if (!Str::startsWith($command, 'screen') && !Str::startsWith($command, 'SCREEN')) {
+                    posix_kill((int) $pid, SIGTERM);
+                }
+            }
         }
     }
 
